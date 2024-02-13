1cee473b8739c1a1923c7f629e522a9a59b33890
Guilherme <sesterhg@amazon.com>
2024-02-13 14:55:27 -0500

    starting repo with sample

--- kms_key/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "Region where to run this TF in"
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment to be used for this run"
-  type        = string
-}
