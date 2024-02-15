48c1f88afd2a84e40c697f94f524dba7c5ecb853
Guilherme <sesterhg@amazon.com>
2024-02-15 12:10:43 -0500

    adding variables for parameters

--- kms_key/modules/kms/variables.tf
+++ kms_key/modules/kms/variables.tf
@@ -1,3 +1,13 @@
+variable "aws_region" {
+  description = "Region where to run this TF in"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment to be used for this run"
+  type        = string
+}
+
 variable "target_service" {
   description = "Service that this KMS is going to be used for"
   type        = string
